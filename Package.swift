26c3bf8a760db5d2a60b86932c8395d633c1da0c
a1ahn <a1ahn@iconloop.com>
2020-07-01 18:17:20 +0900

    Support Swift Package Manager

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ICONKit",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "ICONKit",
+            targets: ["ICONKit-ios"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1"),
+        .package(url: "https://github.com/attaswift/BigInt.git", from: "5.0.0"),
+        .package(url: "https://github.com/greymass/secp256k1.git", from: "0.0.3")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "ICONKit-ios",
+            dependencies: ["BigInt", "CryptoSwift", "secp256k1"]),
+    ]
+)
