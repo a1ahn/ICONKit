9a4e27b5e325f0493d0487401b22c2622d200868
a1ahn <a1ahn@iconloop.com>
2020-07-06 12:31:12 +0900

    Add macos target

---
+++ ICONKit-macos/ICONKit_macos.h
@@ -0,0 +1,19 @@
+//
+//  ICONKit_macos.h
+//  ICONKit-macos
+//
+//  Created by a1ahn on 2020/07/06.
+//  Copyright © 2020 ICON Foundation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ICONKit_macos.
+FOUNDATION_EXPORT double ICONKit_macosVersionNumber;
+
+//! Project version string for ICONKit_macos.
+FOUNDATION_EXPORT const unsigned char ICONKit_macosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ICONKit_macos/PublicHeader.h>
+
+
