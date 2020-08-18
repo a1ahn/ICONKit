1ed016b205705cde31343981ba9420f6c2427a3a
nha1a <gluebox@gmail.com>
2020-08-18 16:23:28 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ICONKit",
-            targets: ["ICONKit-ios"]),
+            targets: ["ICONKit"]),
     ],
     dependencies: [
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.1"),
@@ -20,7 +20,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "ICONKit-ios",
+            name: "ICONKit",
             dependencies: ["BigInt", "CryptoSwift", "secp256k1"],
             path: "Source"
         ),
