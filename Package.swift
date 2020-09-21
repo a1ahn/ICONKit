bc14bf446d6567b286fb4e0e086647425b0fcc8e
nha1a <gluebox@gmail.com>
2020-09-21 12:28:55 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,6 +9,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ICONKit",
+            type: .dynamic,
             targets: ["ICONKit"]),
     ],
     dependencies: [
@@ -22,7 +23,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "ICONKit",
-            dependencies: ["BigInt", "CryptoSwift", "secp256k1", "Scrypt"],
+            dependencies: ["CryptoSwift", "BigInt", "secp256k1", "Scrypt"],
             path: "Source"
         ),
     ]
